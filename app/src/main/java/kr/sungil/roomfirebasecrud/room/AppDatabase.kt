450205bc04ce80915951dd8cc47fa1bae17f1257
SongJuHwan <thdwnghks121@gmail.com>
2023-01-27 15:06:57 +0900

    23-01-27
    last

--- app/src/main/java/kr/sungil/roomfirebasecrud/room/AppDatabase.kt
+++ app/src/main/java/kr/sungil/roomfirebasecrud/room/AppDatabase.kt
@@ -9,6 +9,7 @@ import kr.sungil.roomfirebasecrud.models.BookDTO
 @Database(entities = [BookDTO::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
 	abstract fun bookDao(): BookDAO
+	abstract fun movieDao(): BookDAO
 }
 
 fun getAppDatabase(context: Context): AppDatabase {

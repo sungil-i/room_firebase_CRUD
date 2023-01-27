01eaa0e33ff98c35d273a1240b990f8300a07ac9
박원준 <upj53@sungil-i.kr>
2023-01-27 14:12:45 +0900

    done

--- app/src/main/java/kr/sungil/roomfirebasecrud/MainActivity.kt
+++ app/src/main/java/kr/sungil/roomfirebasecrud/MainActivity.kt
@@ -8,6 +8,7 @@ import kr.sungil.roomfirebasecrud.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 	private lateinit var binding: ActivityMainBinding
 
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityMainBinding.inflate(layoutInflater)
--- app/src/main/java/kr/sungil/roomfirebasecrud/Room1Fragment.kt
+++ app/src/main/java/kr/sungil/roomfirebasecrud/Room1Fragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.UiThread
 import kr.sungil.roomfirebasecrud.Datas.bookItems
 import kr.sungil.roomfirebasecrud.adapters.BookAdapter

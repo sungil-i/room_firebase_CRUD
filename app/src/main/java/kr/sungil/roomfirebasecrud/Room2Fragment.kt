52ba448b3e8cba130c2a6277cc1fff6d653e4a30
박원준 <upj53@sungil-i.kr>
2023-01-30 14:07:59 +0900

    room 2

--- app/src/main/java/kr/sungil/roomfirebasecrud/Room2Fragment.kt
+++ app/src/main/java/kr/sungil/roomfirebasecrud/Room2Fragment.kt
@@ -6,14 +6,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.widget.addTextChangedListener
+import kr.sungil.roomfirebasecrud.adapters.MovieAdapter
 import kr.sungil.roomfirebasecrud.databinding.FragmentRoom2Binding
+import kr.sungil.roomfirebasecrud.room.AppDatabase
+import kr.sungil.roomfirebasecrud.room.getAppDatabase
 
 class Room2Fragment : Fragment(R.layout.fragment_room2) {
 	private var binding: FragmentRoom2Binding? = null
+	private lateinit var db: AppDatabase
+	private lateinit var adpater: MovieAdapter
+
 	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
 		super.onViewCreated(view, savedInstanceState)
 		val _binding = FragmentRoom2Binding.bind(view)
 		binding = _binding
+
+		// Room Database 초기화
+		db = context?.let { getAppDatabase(it) }!!
 
 		initEditText()
 		initButton()
@@ -43,5 +52,9 @@ class Room2Fragment : Fragment(R.layout.fragment_room2) {
 	}
 
 	private fun initRecyclerView() {
+		binding!!.apply {
+
+			rvMovies.adapter
+		}
 	}
 }

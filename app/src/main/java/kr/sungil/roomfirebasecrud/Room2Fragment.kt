2e0a7982a6eddf28d70947a63de2589f1000c053
박원준 <upj53@sungil-i.kr>
2023-01-30 14:38:16 +0900

    room 2

--- app/src/main/java/kr/sungil/roomfirebasecrud/Room2Fragment.kt
+++ app/src/main/java/kr/sungil/roomfirebasecrud/Room2Fragment.kt
@@ -1,6 +1,7 @@
 package kr.sungil.roomfirebasecrud
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -13,51 +14,74 @@ import kr.sungil.roomfirebasecrud.room.AppDatabase
 import kr.sungil.roomfirebasecrud.room.getAppDatabase
 
 class Room2Fragment : Fragment(R.layout.fragment_room2) {
-	private var binding: FragmentRoom2Binding? = null
-	private lateinit var db: AppDatabase
-	private lateinit var adapter: MovieAdapter
-	private val movieList = mutableListOf<MovieDTO>()
+    private var binding: FragmentRoom2Binding? = null
+    private lateinit var db: AppDatabase
+    private lateinit var adapter: MovieAdapter
+    private val movieList = mutableListOf<MovieDTO>()
 
-	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-		super.onViewCreated(view, savedInstanceState)
-		val _binding = FragmentRoom2Binding.bind(view)
-		binding = _binding
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val _binding = FragmentRoom2Binding.bind(view)
+        binding = _binding
 
-		// Room Database 초기화
-		db = context?.let { getAppDatabase(it) }!!
+        // Room Database 초기화
+        db = context?.let { getAppDatabase(it) }!!
 
-		initEditText()
-		initButton()
-		initRecyclerView()
-	}
+        initEditText()
+        initButton()
+        initRecyclerView()
+		readData()
+    }
 
-	private fun initEditText() {
-		binding!!.apply {
-			etTitle.addTextChangedListener {
-				val isOk = etTitle.text.isNotEmpty() && etDirector.text.isNotEmpty()
-				btSave.isEnabled = isOk
-				btCancel.isEnabled = isOk
-			}
-			etDirector.addTextChangedListener {
-				val isOk = etTitle.text.isNotEmpty() && etDirector.text.isNotEmpty()
-				btSave.isEnabled = isOk
-				btCancel.isEnabled = isOk
-			}
-		}
-	}
+    private fun initEditText() {
+        binding!!.apply {
+            etTitle.addTextChangedListener {
+                val isOk = etTitle.text.isNotEmpty() && etDirector.text.isNotEmpty()
+                btSave.isEnabled = isOk
+                btCancel.isEnabled = isOk
+            }
+            etDirector.addTextChangedListener {
+                val isOk = etTitle.text.isNotEmpty() && etDirector.text.isNotEmpty()
+                btSave.isEnabled = isOk
+                btCancel.isEnabled = isOk
+            }
+        }
+    }
 
-	private fun initButton() {
-		binding!!.apply {
-			btSave.isEnabled = false
-			btCancel.isEnabled = false
-		}
-	}
+    private fun initButton() {
+        binding!!.apply {
+            btSave.isEnabled = false
+            btCancel.isEnabled = false
+            btSave.setOnClickListener {
+                val movie = MovieDTO(
+                    adapter.itemCount + 1,
+                    etTitle.text.toString(),
+                    etDirector.text.toString()
+                )
+				Thread {
+					db.movieDao().insertMovie(movie)
+				}
+				readData()
+            }
+        }
+    }
 
-	private fun initRecyclerView() {
-		binding!!.apply {
-			adapter = MovieAdapter(onItemClick = {})
-			rvMovies.adapter
-			adapter.submitList(movieList)
-		}
-	}
+    private fun readData() {
+        var movies: List<MovieDTO>
+        Thread {
+            movies = db.movieDao().getAllMovies()
+            for (m in movies) {
+                movieList.add(m)
+            }
+        }.start()
+        adapter.notifyDataSetChanged()
+    }
+
+    private fun initRecyclerView() {
+        binding!!.apply {
+            adapter = MovieAdapter(onItemClick = {})
+            rvMovies.adapter
+            adapter.submitList(movieList)
+        }
+    }
 }

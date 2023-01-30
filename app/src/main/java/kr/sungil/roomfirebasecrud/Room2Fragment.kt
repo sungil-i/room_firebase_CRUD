29600ccc0764fdfaa1fff4b87787ccabee2211b3
박원준 <upj53@sungil-i.kr>
2023-01-30 14:03:10 +0900

    room 2

--- app/src/main/java/kr/sungil/roomfirebasecrud/Room2Fragment.kt
+++ app/src/main/java/kr/sungil/roomfirebasecrud/Room2Fragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import kr.sungil.roomfirebasecrud.databinding.FragmentRoom2Binding
 
 class Room2Fragment : Fragment(R.layout.fragment_room2) {
@@ -14,5 +15,33 @@ class Room2Fragment : Fragment(R.layout.fragment_room2) {
 		val _binding = FragmentRoom2Binding.bind(view)
 		binding = _binding
 
+		initEditText()
+		initButton()
+		initRecyclerView()
+	}
+
+	private fun initEditText() {
+		binding!!.apply {
+			etTitle.addTextChangedListener {
+				val isOk = etTitle.text.isNotEmpty() && etDirector.text.isNotEmpty()
+				btSave.isEnabled = isOk
+				btCancel.isEnabled = isOk
+			}
+			etDirector.addTextChangedListener {
+				val isOk = etTitle.text.isNotEmpty() && etDirector.text.isNotEmpty()
+				btSave.isEnabled = isOk
+				btCancel.isEnabled = isOk
+			}
+		}
+	}
+
+	private fun initButton() {
+		binding!!.apply {
+			btSave.isEnabled = false
+			btCancel.isEnabled = false
+		}
+	}
+
+	private fun initRecyclerView() {
 	}
 }
